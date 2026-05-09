24ffc18c28d7cd1e2d58cb6e2bfc3963c750af76
wagnerjfjunior <wagnerjfjunior@gmail.com>
2026-05-09 18:55:29 -0300

    feat: route root tenant provisioning by hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import TenantProvisioningStandalone from './components/TenantProvisioningStandalone.jsx'
 import './index.css'
+
+const RootComponent = window.location.hash === '#tenant-provisioning'
+  ? TenantProvisioningStandalone
+  : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>,
 )
